01ddab96f85e3488c4b1b315837d4de8f59d573c
KS9910 <210020021@iitdh.ac.in>
2024-10-28 14:10:09 +0530

    fuction to write data at slave added

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include  <stdint.h>
 
 #include "tm4c123gh6pm.h"
-void I2C_GPIO_CONFG()
+void I2C_GPIO_CONFG(void)
 {
     SYSCTL_RCGCI2C_R |= SYSCTL_RCGCI2C_R0;  // enable clock for I2C
     SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R1;  //enable clock for GPIO
@@ -18,6 +18,30 @@ void I2C_GPIO_CONFG()
 //    I2C0_MSA_R = 0x00000076; //Master address for transmit
 //    I2C0_MDR_R= 0X000000C3;  // Slave address which need to send initially
 //    I2C0_MCS_R = 0x00000007;
+}
+
+void I2C_Write(uint8_t slave_addr, uint8_t reg, uint16_t data) {
+    I2C0_MSA_R = (slave_addr << 1) & 0xFE;  // Set I2C Master Slave Address (write mode)
+
+    // Set data to be sent (DAC register and data)
+    I2C0_MDR_R = reg;                       // DAC register pointer
+    I2C0_MCS_R = 3;                         // Send Start and Run conditions
+
+    // Wait for transmission completion and check errors
+    while (I2C0_MCS_R & 1);                 // Wait for transmission to complete
+    if (I2C0_MCS_R & 0xE) return;           // Return on error
+
+    I2C0_MDR_R = (data >> 8) & 0xFF;        // Send the MSB first
+    I2C0_MCS_R = 1;                         // Run without start/stop
+
+    while (I2C0_MCS_R & 1);                 // Wait for transmission to complete
+    if (I2C0_MCS_R & 0xE) return;           // Return on error
+
+    I2C0_MDR_R = data & 0xFF;               // Send the LSB
+    I2C0_MCS_R = 5;                         // Run and Stop conditions
+
+    while (I2C0_MCS_R & 1);                 // Wait for transmission to complete
+    if (I2C0_MCS_R & 0xE) return;           // Return on error
 }
 
 
